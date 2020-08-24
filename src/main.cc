f09f9de9b69fa74f5ec9c46df2e54219c225c8c1
Tiny Paws <bastienmillet073@gmail.com>
2020-08-24 14:37:53 +0200

    Adding a bunch of ideas

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "repl.h"
 
+// To implement
+// Doubly linked lists
+// Radix tree
+// Red-Black tree
+// AVL tree
+// T-Tree
+// B-Tree
+// B+Tree ??
+//
+// ideas
+// sorting linked list
+// checking if a bt is a bst
+// converting bt to bst
+// converting linked list to doubly linked list
+// saving datastructures
+// importing datastructures
+
 auto main(void) -> int {
   repl::run();
 }
